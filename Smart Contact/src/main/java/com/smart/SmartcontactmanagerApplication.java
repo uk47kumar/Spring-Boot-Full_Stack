f20b493e339c6e1fd80753ed0bf7f18e31543db5
Gaurav Sharma <gauravsharma049049@gmail.com>
2022-10-16 03:09:35 +0530

    first commit

--- Smart Contact/src/main/java/com/smart/SmartcontactmanagerApplication.java
+++ Smart Contact/src/main/java/com/smart/SmartcontactmanagerApplication.java
@@ -2,12 +2,19 @@ package com.smart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SmartcontactmanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartcontactmanagerApplication.class, args);
 	}
-
+	@RequestMapping("/")
+	public String home(){
+		//creating endpoints
+		return "welcome to springboot project home";
+	}
 }
