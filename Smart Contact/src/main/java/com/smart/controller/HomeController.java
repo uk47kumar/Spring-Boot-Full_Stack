a3357cfdae58b372b65cb3e19cd5b2af02e7df13
uk47kumar <uk47kumar@gmail.com>
2022-11-03 22:55:34 +0530

    modify front

--- Smart Contact/src/main/java/com/smart/controller/HomeController.java
+++ Smart Contact/src/main/java/com/smart/controller/HomeController.java
@@ -22,56 +22,55 @@ public class HomeController {
     private UserRepository userRepository;
 
     @RequestMapping("/")
-    public String home(Model model){
-        model.addAttribute("title","Home-Smart Contact Manager");
+    public String home(Model model) {
+        model.addAttribute("title", "Home-Smart Contact Manager");
         return "home";
     }
+
     @RequestMapping("/about")
-    public String about(Model model){
-        model.addAttribute("title","About-Smart Contact Manager");
+    public String about(Model model) {
+        model.addAttribute("title", "About-Smart Contact Manager");
         return "about";
     }
 
     @RequestMapping("/signup")
-    public String signup(Model model){
-        model.addAttribute("title","Register-Smart Contact Manager");
+    public String signup(Model model) {
+        model.addAttribute("title", "Register-Smart Contact Manager");
         model.addAttribute("user", new User());
         return "signup";
     }
 
-//    handel user registration
+    //    handel user registration
     @PostMapping("/do_register")
-    public String registerUser(@ModelAttribute("user")User user,
-                               @Valid
+    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult,
                                @RequestParam(value = "agreement", defaultValue = "false") boolean agreement,
-                               Model model, BindingResult bindingResult, HttpSession session){
+                               Model model, HttpSession session) {
         try {
-            if (!agreement){
+            if (!agreement) {
                 System.out.println("you have not agreed the term and conditions");
                 throw new Exception("you have not agreed the term and conditions");
             }
-            if (bindingResult.hasErrors()){
-                System.out.println("ERROR "+ bindingResult.toString());
-                model.addAttribute("user",user);
+            if (bindingResult.hasErrors()) {
+                System.out.println("ERROR " + bindingResult.toString());
+                model.addAttribute("user", user);
                 return "signup";
             }
             user.setRole("ROLE_USER");
             user.setEnabled(true);
             user.setImageUrl("default.png");
 
-            System.out.println("Agreement "+ agreement);
+            System.out.println("Agreement " + agreement);
             System.out.println("USER " + user);
 
             User result = this.userRepository.save(user);
             model.addAttribute("user", new User());
 
-            session.setAttribute("message",new Message("Successfully Registered !!","alert-success"));
+            session.setAttribute("message", new Message("Successfully Registered !!", "alert-success"));
             return "signup";
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            model.addAttribute("user",user);
-            session.setAttribute("message",new Message("Something went wrong "+ e.getMessage(),"alert-danger"));
+            model.addAttribute("user", user);
+            session.setAttribute("message", new Message("Something went wrong " + e.getMessage(), "alert-danger"));
             return "signup";
         }
     }
